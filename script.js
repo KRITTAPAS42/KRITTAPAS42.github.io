c28b410529425e015395587e82b3e6d8d2259e46
KRITTAPAS42 <22166@satitpatumwan.ac.th>
2024-10-12 18:36:31 +0700

    new ver change >

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ console.log("Successful");
 function navigateToChapter1() {
     window.location.href = 'chapter1.html';
 }
+
+function navigateToChapter2() {
+    window.location.href = 'chapter2.html';
+}
+
+function navigateToChapter3() {
+    window.location.href = 'chapter3.html';
+}
+
+function navigateToChapter4() {
+    window.location.href = 'chapter4.html';
+}
